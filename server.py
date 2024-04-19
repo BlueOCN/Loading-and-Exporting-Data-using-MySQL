96366379efc70436c40d3138d6ad3adc0fab5403
BlueOCN <robgonzalezmu@gmail.com>
2024-04-18 19:12:16 -0600

    added more text

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ class HTTPHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        with open('/home/coder/project/actors.csv') as f:
+        with open('Solar Eclipses.csv') as f:
             reader = csv.reader(f)
             self.wfile.write(b"\n".join([','.join(row).encode() for row in reader]))
 
